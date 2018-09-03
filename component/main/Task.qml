d57c8a74b1f24ef9051a842712b32359e16554ad
qinyinghao <jsqinyinghao@live.com>
2018-09-03 14:08:00 +0800

    任务列表的展开和排序，未完成

--- component/main/Task.qml
+++ component/main/Task.qml
@@ -1,20 +1,100 @@
 import QtQuick 2.9
 import QtQuick.Controls 2.2
+import QtQml.Models 2.11
 import "../common" as COMMON
 
 Page{
     id:taskView
 
-    Label{
-        text: "TASK"
-        font.pointSize: 30
-        font.bold: true
-        anchors.centerIn: parent
+    //任务列表的model
+    ListModel{
+        id:tasklists
+    }
+
+    //任务列表
+    ListView {
+        id: view
+        anchors {
+            left: parent.left; top: parent.top; right: parent.right; bottom: parent.bottom;
+            margins: 2
+        }
+        model: visualModel//指向可visualModel(排序后的) 而非 tasklists（未排序的）
+
+        spacing: 4
+        cacheBuffer: 50
+    }
+
+    DelegateModel {
+        id: visualModel
+
+        function lessThan(left, right) {
+            //先按照状态排序
+            if(left.status < right.status)return true;
+            //再按照优先级排序
+            if(left.priority > right.priority)return true;
+            //再按照id排序
+            if(left.id < right.id)return true;
+
+            return false;
+        }
+
+        Component.onCompleted:items.setGroups(0, items.count, "unsorted")
+
+        function insertPosition(item) {
+            var lower = 0
+            var upper = items.count
+            while (lower < upper) {
+                var middle = Math.floor(lower + (upper - lower) / 2)
+                var result = visualModel.lessThan(item.model, items.get(middle).model);
+                if (result) {
+                    upper = middle
+                } else {
+                    lower = middle + 1
+                }
+            }
+            return lower
+        }
+
+        function sort() {
+            while (unsortedItems.count > 0) {
+                var item = unsortedItems.get(0)
+                var index = insertPosition(item)
+
+                item.groups = "items"
+                items.move(item.itemsIndex, index)
+            }
+        }
+
+        items.includeByDefault: false
+
+        groups: VisualDataGroup {
+            id: unsortedItems
+            name: "unsorted"
+            includeByDefault: true
+            onChanged: {
+                visualModel.sort()
+            }
+        }
+
+        model: tasklists
+
+        delegate: TaskDelegate {
+            id: delegate
+            width: visualModel.width
+        }
+
     }
 
     function init()
     {
-        //TODO
+
     }
 
+    function updateItems()
+    {
+        //TODO 更新listmodel
+
+        //重新排序
+        visualModel.items.setGroups(0, items.count, "unsorted")
+    }
 }
--- taskmodeldata.h
+++ taskmodeldata.h
@@ -25,9 +25,67 @@ class TaskModelData : public QObject
 public:
     explicit TaskModelData(QObject *parent = nullptr);
 
+    int id(){return m_id;}
+    int excuteAgv(){return m_excuteAgv;}
+    int priority(){return m_priority;}
+    int status(){return m_status;}
+    QString produceTime(){return m_produceTime;}
+    QString doTime(){return m_doTime;}
+    QString doneTime(){return m_doneTime;}
+    QString cancelTime(){return m_cancelTime;}
+    QString errorTime(){return m_errorTime;}
+    QString errorCode(){return m_errorCode;}
+    QString errorInfo(){return m_errorInfo;}
+    bool isCancel(){return m_isCancel;}
+    QString discribe(){return m_discribe;}
+
+
+    void setId(int _id){m_id = _id;emit idChanged(_id);}
+    void setExcuteAgv(int _excuteAgv){m_excuteAgv = _excuteAgv;emit excuteAgvChanged(_excuteAgv);}
+    void setPriority(int _priority){m_priority = _priority;emit priorityChanged(_priority);}
+    void setStatus(int _status){m_status = _status;emit statusChanged(_status);}
+    void setProduceTime(QString _produceTime){m_produceTime = _produceTime;emit produceTimeChanged(_produceTime);}
+    void setDoTime(QString _doTime){m_doTime = _doTime;emit doTimeChanged(_doTime);}
+    void setDoneTime(QString _doneTime){m_doneTime = _doneTime;emit doneTimeChanged(_doneTime);}
+    void setCancelTime(QString _cancelTime){m_cancelTime = _cancelTime;emit cancelTimeChanged(_cancelTime);}
+    void setErrorTime(QString _errorTime){m_errorTime = _errorTime;emit errorTimeChanged(_errorTime);}
+    void setErrorCode(QString _errorCode){m_errorCode = _errorCode;emit errorCodeChanged(_errorCode);}
+    void setErrorInfo(QString _errorInfo){m_errorInfo = _errorInfo;emit errorInfoChanged(_errorInfo);}
+    void setIsCancel(bool _isCancel){m_isCancel = _isCancel;emit isCancelChanged(_isCancel);}
+    void setDiscribe(QString _discribe){m_discribe = _discribe;emit discribeChanged(_discribe);}
+
+
 signals:
+    void idChanged(int _id);
+    void excuteAgvChanged(int _excuteAgv);
+    void priorityChanged(int _priority);
+    void statusChanged(int _status);
+    void produceTimeChanged(QString _produceTime);
+    void doTimeChanged(QString _doTime);
+    void doneTimeChanged(QString _doneTime);
+    void cancelTimeChanged(QString _cancelTime);
+    void errorTimeChanged(QString _errorTime);
+    void errorCodeChanged(QString _errorCode);
+    void errorInfoChanged(QString _errorInfo);
+    void isCancelChanged(bool _isCancel);
+    void discribeChanged(QString _discribe);
 
 public slots:
+
+private:
+    int m_id;
+    int m_excuteAgv;
+    int m_priority;
+    int m_status;
+    QString m_produceTime;
+    QString m_doTime;
+    QString m_doneTime;
+    QString m_cancelTime;
+    QString m_errorTime;
+    QString m_errorCode;
+    QString m_errorInfo;
+    bool m_isCancel;
+    QString m_discribe;
 };
 
 #endif // TASKMODELDATA_H
